422d25c0bedf7cf73b6351703efb0181c892eb46
root <root@33184.example.com>
2025-05-16 17:11:53 +0200

    fix elery_worker

--- worker/celery_worker.py
+++ worker/celery_worker.py
@@ -1,19 +1,18 @@
-import sys
 import os
+import sys
+from celery import Celery
+from worker.stylizer import stylize_video
+from pdf.comic_maker import make_comic
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from celery import Celery
-
 celery_app = Celery(
-    "worker", broker="redis://redis:6379/0", backend="redis://redis:6379/0"
+    "worker",
+    broker="redis://redis:6379/0",
+    backend="redis://redis:6379/0"
 )
-
 
 @celery_app.task(name="worker.process_video")
 def process_video(video_path):
-    from worker.stylizer import stylize_video
-    from pdf.comic_maker import make_comic
-
     frames = stylize_video(video_path)
     return make_comic(frames, video_path)
